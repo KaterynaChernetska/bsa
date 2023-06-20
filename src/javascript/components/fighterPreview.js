188f7fcd98174931122b018086fd5097f5a749e4
Kate <vesnavved@gmail.com>
2023-06-20 12:25:28 +0300

    feat: + add fighters preview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,17 +1,5 @@
 import createElement from '../helpers/domHelper';
 
-export function createFighterPreview(fighter, position) {
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
-
-    // todo: show fighter info (image, name, health, etc.)
-
-    return fighterElement;
-}
-
 export function createFighterImage(fighter) {
     const { source, name } = fighter;
     const attributes = {
@@ -26,4 +14,25 @@ export function createFighterImage(fighter) {
     });
 
     return imgElement;
+}
+export function createFighterPreview(fighter, position) {
+    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+    const fighterElement = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root ${positionClassName}`
+    });
+    if (fighter) {
+        const imageElement = createFighterImage(fighter);
+        const fighterInfo = createElement({ tagName: 'ul', className: 'fighter-preview__info' });
+        fighterInfo.innerHTML = `<li>Name: ${fighter.name}</li>
+        <li>Health: ${fighter.health}</li>
+        <li>Defense: ${fighter.defense}</li>
+        <li>Attack: ${fighter.attack}</li>`;
+
+        fighterElement.append(imageElement);
+        fighterElement.append(fighterInfo);
+    }
+    // todo: show fighter info (image, name, health, etc.)
+
+    return fighterElement;
 }

1e2f6440350351525e4b52640e7fdae77b83e847
Kate <vesnavved@gmail.com>
2023-06-21 21:38:15 +0300

    feat: + getHitPower, getBlockPower, getDamage functions

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,19 +1,28 @@
-import controls from '../../constants/controls';
+/* eslint-disable default-case */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-param-reassign */
 
 export async function fight(firstFighter, secondFighter) {
     return new Promise(resolve => {
-        // resolve the promise with the winner when fight is over
+        if (secondFighter.health <= 0) resolve(firstFighter);
+        resolve(secondFighter);
     });
 }
 
-export function getDamage(attacker, defender) {
-    // return damage
-}
-
-export function getHitPower(fighter) {
+export function getHitPower({ attack }) {
+    const criticalHitChance = Math.random() + 1;
+    return attack * criticalHitChance;
     // return hit power
 }
 
-export function getBlockPower(fighter) {
+export function getBlockPower({ defense }) {
+    const dodgeChance = Math.random() + 1;
+    return defense * dodgeChance;
     // return block power
+}
+
+export function getDamage(attacker, defender) {
+    const damage = getHitPower(attacker) - getBlockPower(defender);
+    return damage > 0 ? damage : 0;
+    // return damage
 }
